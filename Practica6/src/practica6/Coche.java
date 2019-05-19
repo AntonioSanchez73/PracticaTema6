ee708e33abf6e14b366b813c626051cd8d3a9076
AntonioSnachez <AntonioSanchez@AntonioSanchez>
2019-05-19 17:12:31 +0200

    Ejercicio 3: Documentación. Closes #3

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -21,6 +21,12 @@ public class Coche {
 		fabricante = f;
 	}
 
+	/** METODO 1. 
+	* Según el valor de la variable combustible se le dará un valor diferente a la
+	* la variable resultado, usando para ello las funciones cocheMatricula() y metodoA/B/C().
+	* @return Devuelve la variable resultado.
+	*/
+	
 	public String metodo1()
 	{
 		String resultado = "";
@@ -46,6 +52,11 @@ public class Coche {
 		return resultado;
 	}
 	
+	/** METODO A. 
+	* Llama a la función coche() para completar una cadena de texto.
+	* @return Una cadena de texto completada con lo que devuelve la función coche().
+	*/
+	
 	public String metodoA(String modelo, String fabricante)
 	{
 		modelo = "Corsa";
@@ -53,6 +64,11 @@ public class Coche {
 		return coche(modelo, fabricante) + "y gasta 1,337 euros por litro.";
 	}
 	
+	/** METODO B. 
+	* Llama a la función coche() para completar una cadena de texto.
+	* @return Una cadena de texto completada con lo que devuelve la función coche().
+	*/
+
 	public String metodoB(String modelo, String fabricante)
 	{
 		modelo = "Seat";
@@ -60,15 +76,29 @@ public class Coche {
 		return coche(modelo, fabricante) + "y gasta 1,052 euros por litro.";
 	}
 	
+	/** METODO C. 
+	* Llama a la función coche() para completar una cadena de texto.
+	* @return Una cadena de texto completada con lo que devuelve la función coche().
+	*/
+	
 	public String metodoC(String modelo, String fabricante)
 	{
 		modelo = "Hyundai";
 		fabricante = "Getz";
 		return coche(modelo, fabricante) + "y no necesita combustible.";
 	}
+	
+	/** COCHE MATRICULA. 
+	* @return Una cadena formada por la constante COCHE_MATRICULA + matricula.
+	*/
+	
 	private String cocheMatricula() {
 		return COCHE_MATRICULA + matricula + " ";
 	}
+	
+	/** COCHE. 
+	* @return Una cadena formada por alguna cadena de texto + las variables fabricante y modelo.
+	*/
 	
 	private String coche(String modelo, String fabricante) {
 		return "Es un " + fabricante + " " + modelo;
